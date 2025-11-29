3d50a7322e74b6b0b47c7f037089c40cc3339937
Bao To <baotoq@outlook.com>
2025-11-29 15:38:58 +0700

    update

--- TradingBot.AppHost/AppHost.cs
+++ TradingBot.AppHost/AppHost.cs
@@ -5,12 +5,4 @@ var cache = builder.AddRedis("cache");
 var apiService = builder.AddProject<Projects.TradingBot_ApiService>("apiservice")
     .WithHttpHealthCheck("/health");
 
-builder.AddProject<Projects.TradingBot_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
-    .WithHttpHealthCheck("/health")
-    .WithReference(cache)
-    .WaitFor(cache)
-    .WithReference(apiService)
-    .WaitFor(apiService);
-
 builder.Build().Run();
