59996c7d35de7325e4a7fe97f283cb0942f7ac71
Bao To <baotoq@outlook.com>
2026-02-20 22:13:38 +0700

    update config

--- TradingBot.Mobile/lib/core/api/config.dart
+++ TradingBot.Mobile/lib/core/api/config.dart
@@ -2,7 +2,7 @@
 /// Usage: flutter run --dart-define=API_BASE_URL=http://192.168.1.100:5000 --dart-define=API_KEY=your-key
 const String kApiBaseUrl = String.fromEnvironment(
   'API_BASE_URL',
-  defaultValue: 'http://localhost:5000',
+  defaultValue: 'http://apiservice-tradingbot.dev.localhost:5459',
 );
 
 const String kApiKey = String.fromEnvironment(
